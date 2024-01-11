e1c96de901607563fee405df436c6c19dfa338b0
ELMEHDI_ELFARI <144785903+ElmehdiElfari@users.noreply.github.com>
2024-01-11 15:35:23 +0100

    add article controller

--- controllers/addarticle_controller.php
+++ controllers/addarticle_controller.php
@@ -1,5 +1,5 @@
 <?php
-$article = new Article;
+$article = new Article('articles');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['articleSubmit'])) {
     extract($_POST);
@@ -7,4 +7,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['articleSubmit'])) {
     $articleModel->insertArticle($title, $content, $tags, $categories);
     header("Location: index.php?wiki");
     exit();
+}
+
+$articles = $article->getAllArticles();
+
+
+if ($articles) {
+    foreach ($articles as $article) {
+        $affiche = "<h2>" . $article['article_title'] . "</h2>";
+        $affiche = "<p>" . $article['article_content'] . "</p>";
+        $affiche = "<p>Tags: " . $article['article_tags'] . "</p>";
+        $affiche = "<p>Category: " . $article['article_categorie'] . "</p>";
+        "<hr>";
+    }
+} else {
+    echo "No articles found.";
 }

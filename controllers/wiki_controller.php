c82eac483f0b771930bc6a49719893429e1f1b74
ELMEHDI_ELFARI <144785903+ElmehdiElfari@users.noreply.github.com>
2024-01-12 00:13:48 +0100

    wiki controller

--- controllers/wiki_controller.php
+++ controllers/wiki_controller.php
@@ -2,3 +2,20 @@
 $article = new Article('articles');
 
 $articles = $article->getAllArticles();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['articleDelete'])) {
+    extract($_POST);
+
+    $articleModel = new WikiModel($article);
+    $articleModel->deleteArticle($articleId);
+    header("Location: index.php?addarticle");
+    exit();
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['articleDelete'])) {
+    extract($_POST);
+
+    $articleModel = new WikiModel($articleId, $title, $content, $tags, $categories);
+    $articleModel->updateArticle($articleId, $title, $content, $tags, $categories);
+    header("Location: index.php?addarticle");
+    exit();
+}

20d87798aeefc9f9f85612cf2554f67007b371b3
ELMEHDI_ELFARI <144785903+ElmehdiElfari@users.noreply.github.com>
2024-01-11 15:37:00 +0100

    article classes

--- classes/Article.php
+++ classes/Article.php
@@ -2,6 +2,11 @@
 
 class Article extends Database
 {
+    public function __construct($tableName)
+    {
+        $this->tableName = $tableName;
+        parent::__construct();
+    }
 
     /** Class Method Inserer Les Article:
      * @param string $title
@@ -11,12 +16,47 @@ class Article extends Database
      */
     public function insert($title, $content, $tags, $categories)
     {
-        $sql = "INSERT INTO articles (article_title, article_content, article_tags, article_categorie) VALUES (:title, :content, :tags, :categories)";
-        $stmt = $this->dbh->prepare($sql);
-        $stmt->bindParam(':title', $title);
-        $stmt->bindParam(':content', $content);
-        $stmt->bindParam(':tags', $tags);
-        $stmt->bindParam(':categories', $categories);
-        $stmt->execute();
+        $this->query("INSERT INTO $this->tableName (article_title, article_content, article_tags, article_categorie) VALUES (:title, :content, :tags, :categories)");
+        $this->bind(':title', $title);
+        $this->bind(':content', $content);
+        $this->bind(':tags', $tags); // Corrected the typo here
+        $this->bind(':categories', $categories);
+        $this->execute();
+    }
+
+    /** Retrieves all articles from the database
+     * @return array Associative array containing all articles
+     */
+    public function getAllArticles()
+    {
+        $this->query("SELECT * FROM $this->tableName");
+        $this->execute();
+        return $this->resultSet();
+    }
+
+    public function getArticleById($articleId)
+    {
+        $this->query("SELECT * FROM $this->tableName WHERE id = :id");
+        $this->bind(':id', $articleId);
+        $this->execute();
+        return $this->single();
+    }
+
+    public function updateArticle($articleId, $title, $content, $tags, $categories)
+    {
+        $this->query("UPDATE $this->tableName SET article_title = :title, article_content = :content, article_tags = :tags, article_categorie = :categories WHERE id = :id");
+        $this->bind(':id', $articleId);
+        $this->bind(':title', $title);
+        $this->bind(':content', $content);
+        $this->bind(':tags', $tags);
+        $this->bind(':categories', $categories);
+        $this->execute();
+    }
+
+    public function deleteArticle($articleId)
+    {
+        $this->query("DELETE FROM $this->tableName WHERE id = :id");
+        $this->bind(':id', $articleId);
+        $this->execute();
     }
 }

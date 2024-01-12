2f2f131584f4a5a9be99c548199ae2e2755a3fbd
ELMEHDI_ELFARI <144785903+ElmehdiElfari@users.noreply.github.com>
2024-01-12 17:04:20 +0100

    layout

--- views/_layout.php
+++ views/_layout.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <title><?= ucfirst($page) ?></title>
     <style>
         table,
@@ -28,6 +29,8 @@
     </main>
 
     <footer></footer>
+    <script src="elmehdi_elfari_Wiki/assets/js/main.js"></script>
+
 </body>
 
 </html>

be26e052c121929a021518b81c23b78f4325874e
ELMEHDI_ELFARI <144785903+ElmehdiElfari@users.noreply.github.com>
2024-01-14 17:00:49 +0100

    tag controller

--- controllers/tags_controller.php
+++ controllers/tags_controller.php
@@ -2,7 +2,6 @@
 
 $tag = new Tag('tags');
 $tagmodel = new TagModel($tag);
-// $tag->insert($tag_name);
 
 $tags = $tagmodel->gettag();
 
@@ -17,7 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['tagDelete'])) {
     extract($_POST);
 
     $tagModel = new TagModel($tag);
-    $tagModel->deleteTag($tagId);
+    $tagModel->deleteTags($tagId);
     header("Location: index.php?tags");
     exit();
 }
@@ -25,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['updateTag'])) {
     extract($_POST);
 
     $tagModel = new TagModel($tag);
-    $tagModel->updateTag($tageId, $tag_name);
+    $tagModel->updateTags($tagId, $tag_name);
     header("Location: index.php?page=tags");
     exit();
 }

9ec1f539f989bdfa754febfe6d89920f0af33fcf
ELMEHDI_ELFARI <144785903+ElmehdiElfari@users.noreply.github.com>
2024-01-14 20:04:44 +0100

    dash model

--- models/dashboard_model.php
+++ models/dashboard_model.php
@@ -12,4 +12,9 @@ class dashboardModel
     {
         return $this->article->countAllArticles();
     }
+
+    public function getArticles()
+    {
+        return $this->article->getAllArticles();
+    }
 }

8a4a0292d620c8388020cf43141c6c79f80ab6fe
ELMEHDI_ELFARI <144785903+ElmehdiElfari@users.noreply.github.com>
2024-01-14 13:28:34 +0100

    classes js

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-
-function toggleContent(button) {
-    const card = button.closest('.card');
-    const content = card.querySelector('.card-content');
-    content.style.display = (content.style.display === 'none') ? 'block' : 'none';
-}
-console.log("hhhhhh");

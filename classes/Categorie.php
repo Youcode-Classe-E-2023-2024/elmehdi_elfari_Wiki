ff104e1c0f8fd0cdb407cec356e1fde709fed09d
ELMEHDI_ELFARI <144785903+ElmehdiElfari@users.noreply.github.com>
2024-01-14 13:29:19 +0100

    classes categorie

---
+++ classes/Categorie.php
@@ -0,0 +1,30 @@
+<?php
+
+class Categorie extends Database
+{
+    public function __construct($tableName)
+    {
+        $this->tableName = $tableName;
+        parent::__construct();
+    }
+
+
+    public function insert($categorie_name)
+    {
+        $this->query("INSERT INTO $this->tableName (categorie_name) VALUES (:categorie_name)");
+        $this->bind(':categorie_name', $categorie_name);
+        $this->execute();
+    }
+    public function deleteCategorie($categorie_id)
+    {
+        $this->query("DELETE FROM $this->tableName WHERE categorie_id  = :id");
+        $this->bind(':id', $categorie_id);
+        $this->execute();
+    }
+    public function getCategorie()
+    {
+        $this->query("SELECT * FROM $this->tableName");
+        $this->execute();
+        return $this->resultSet();
+    }
+}
